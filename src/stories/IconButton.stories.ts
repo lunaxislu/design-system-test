e2edc5c05c106d015bdf1084872984d387341fa1
lunaxislu <mimk0905@gmail.com>
2026-01-07 00:15:43 +0900

    test failed

--- src/stories/IconButton.stories.ts
+++ src/stories/IconButton.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
-import { IconButton } from "../components/IconButton";
+import { IconButton } from "@/components/IconButton";
 
 import { fn } from "storybook/test";
 const meta = {
